7070aed163aea76d6892ebe44197f780e26abf4b
Wuu-laptop <1249732457@qq.com>
2021-06-09 19:07:47 +0800

    📝 基本应用类型

--- 3-code-professional-javascrpt-for-web-develeopment/5-basic-reference-types/1-types.js
+++ 3-code-professional-javascrpt-for-web-develeopment/5-basic-reference-types/1-types.js
@@ -256,26 +256,263 @@
 
 
 // 提取子字符串
-let str = "hello world"
-// console.log(str.slice(3)); // lo world
-// console.log(str.substring(3));// lo world
-// console.log(str.substr(3));// lo world
-// console.log(str.slice(3, 7)); // lo w
-// console.log(str.substring(3, 7));// lo w
-// console.log(str.substr(3, 7));// lo worl 这里不同是因为，第二个参数，表示截取7个字符串
+// let str = "hello world"
+// // console.log(str.slice(3)); // lo world
+// // console.log(str.substring(3));// lo world
+// // console.log(str.substr(3));// lo world
+// // console.log(str.slice(3, 7)); // lo w
+// // console.log(str.substring(3, 7));// lo w
+// // console.log(str.substr(3, 7));// lo worl 这里不同是因为，第二个参数，表示截取7个字符串
 
-console.log(str.slice(-3)); // rld  -3 --> 8
-console.log(str.substring(-3));// hello world  -3 --> 0
-console.log(str.substr(-3));// rld  -3 --> 8
-console.log(str.slice(3, -4)); // lo w  -4 --> 7  -4+长度=7
-console.log(str.substring(3, -4));// hel -4 --> 0  (3,0) -->(0,3)
-console.log(str.substr(3, -4));// '' -4 --> 0
+// console.log(str.slice(-3)); // rld  -3 --> 8
+// console.log(str.substring(-3));// hello world  -3 --> 0
+// console.log(str.substr(-3));// rld  -3 --> 8
+// console.log(str.slice(3, -4)); // lo w  -4 --> 7  -4+长度=7
+// console.log(str.substring(3, -4));// hel -4 --> 0  (3,0) -->(0,3)
+// console.log(str.substr(3, -4));// '' -4 --> 0
 
-console.log(str.slice(3, -1)); // lo worl
-console.log(str.substring(3, -1));// hel
-console.log(str.substr(3, -1));// ''
+// console.log(str.slice(3, -1)); // lo worl
+// console.log(str.substring(3, -1));// hel
+// console.log(str.substr(3, -1));// ''
 
-console.log(str.slice(-3, -1)); // rl
-console.log(str.substring(-3, -1));// ''  (0,0)
-console.log(str.substr(-3, -1));// ''  (0,0)
+// console.log(str.slice(-3, -1)); // rl
+// console.log(str.substring(-3, -1));// ''  (0,0)
+// console.log(str.substr(-3, -1));// ''  (0,0)
 // 总结 后两个遇到负数都会转成0
+
+
+// 字符串位置方法
+// let str = 'hello world'
+// console.log(str.indexOf('o')); // 4
+// console.log(str.lastIndexOf('o')); // 7
+// // 第二个参数
+// console.log(str.indexOf('o', 6)); // 7 从6向后找
+// console.log(str.lastIndexOf('o', 6)); // 4 从6向前前找
+
+// let str = 'lorem ipsum sit amet,consectetur adipiscing elit'
+// let positions = []
+// let pos = str.indexOf('e')
+// while (pos > -1) {
+//   positions.push(pos)
+//   pos = str.indexOf('e', pos + 1)
+// }
+// console.log(positions);
+
+
+// 字符串包含方法
+// let message = 'foobarbaz'
+// console.log(message.startsWith('foo')); // true
+// console.log(message.startsWith('bar')); // false
+// console.log(message.endsWith('baz')); // true
+// console.log(message.endsWith('bar')); // false
+// console.log(message.includes('bar')); // true
+// console.log(message.includes('oba')); // true
+
+
+// trim()
+// let str = '   hello value   '
+// console.log(str.trim()); // hello value
+
+
+// repeate()
+// let str = 'na '
+// console.log(str.repeat(16) + 'batman');
+
+
+// padStrat()和padEnd()
+// let str = 'foo'
+// console.log(str.padStart(6));  //   foo
+// console.log(str.padStart(6, '.')); // ...foo
+// console.log(str.padEnd(6)); // foo   
+// console.log(str.padEnd(6, '.')); // foo...
+
+// console.log(str.padStart(7, 'bar')); // barbfoo
+// console.log(str.padEnd(7, 'bar')); // foobarb
+
+
+// let message = 'abc'
+// let strIterator = message[Symbol.iterator]()
+// console.log(strIterator.next()); // { value: 'a', done: false }
+// console.log(strIterator.next()); // { value: 'b', done: false }
+// console.log(strIterator.next()); // { value: 'c', done: false }
+// console.log(strIterator.next()); // { value: undefined, done: true }
+
+// for (const v of 'hello') {
+//   console.log(v);
+// }
+
+// console.log([...'hello']); // [ 'h', 'e', 'l', 'l', 'o' ]
+
+
+// 大小写转换
+// let str = 'Hello World'
+// console.log(str.toLocaleUpperCase()); //HELLO WORLD
+// console.log(str.toUpperCase());
+// console.log(str.toLocaleLowerCase()); //hello world
+// console.log(str.toLowerCase());
+
+
+
+// 正则匹配
+// let text = 'cat,bat,sat,fat'
+// let pattern = /.at/
+// // 等价于pattern.exec(text),还是不一样的,如flags就用不了
+// let matches = text.match(pattern)
+// console.log(matches.index); // 0
+// console.log(matches[0]); // cat
+// console.log(pattern.lastIndex); // 0
+
+
+// search()，参数正则，返回第一个字符在字符串中的位置
+// let text = 'cat,bat,sat,fat'
+// pos = text.search(/bat/)
+// console.log(pos); // 4
+
+
+// replace(),第一个参数接受正则或者字符串(因此字符串不会被转换为正则)
+// let text = 'cat,bat,sat,fat'
+// let result1 = text.replace(/.at/, 'foo')
+// let result2 = text.replace('.at', 'foo')
+// let result3 = text.replace(/.at/g, 'foo')
+// console.log(result1); // foo,bar,sat,fat 只替换了第一个子字符串
+// console.log(result2); // cat,bar,sat,fat 因为字符串没有转换为正则所以匹配
+// console.log(result3); // foo foo foo foo 全局替换
+
+
+// let text = 'cat,bat,sat,fat'
+// const result = text.replace(/(.at)/g, "word($1)")
+// console.log(result); // word(cat),word(bat),word(sat),word(fat)
+
+
+// 函数参数
+// function changeChar(text) {
+//   // 第二个参数是函数时，此函数有三个参数：匹配的字符串，匹配字符串的开始位置，整个字符串
+//   return text.replace(/[<>"&]/g, (match, pos, originalText) => {
+//     switch (match) {
+//       case '<':
+//         return '&lt'
+//       case '>':
+//         return '&rt'
+//       default:
+//         return '☺'
+//     }
+//   })
+// }
+// console.log(changeChar('<1> " <2> & <3> " <4>'));// &lt1&rt ☺ &lt2&rt ☺ &lt3&rt ☺ &lt4&rt
+
+
+//split()
+// let colorText = 'red,blue,green,yellow'
+// const colors1 = colorText.split(',') // [ 'red', 'blue', 'green', 'yellow' 
+// const colors2 = colorText.split(',', 2) // [ 'red', 'blue' ]
+// const colors3 = colorText.split(/[^,]+/) // [^,]+ 匹配除了，的字符串
+// console.log(colors3);// [ '', ',', ',', ',', '' ]
+
+
+
+// localeCompare()
+// let str = 'yellow'
+// console.log(str.localeCompare('black')); // 1
+// console.log(str.localeCompare('yellow')); //0
+// console.log(str.localeCompare('zoo')); // -1
+// console.log(str.localeCompare('Yellow')); // -1 ,跟地区相关，不一定是-1
+
+
+
+
+// encodeURI() encodeURIComponent()
+// let uri = 'http://www.localhost:9000/illeagel value.js#start'
+// // http://www.localhost:9000/illeagel%20value.js#star  只编码不属于URL组件的特殊字符
+// console.log(encodeURI(uri)); 
+// // http%3A%2F%2Fwww.localhost%3A9000%2Filleagel%20value.js%23star  编码所有发现的非标准字符
+// console.log(encodeURIComponent(uri));
+
+// decodeURI() && decodeURIComponent()
+// let url = 'http%3A%2F%2Fwww.localhost%3A9000%2Filleagel%20value.js%23star'
+// // http%3A%2F%2Fwww.localhost%3A9000%2Filleagel value.js%23sta
+// console.log(decodeURI(url));
+// // http://www.localhost:9000/illeagel value.js#star
+// console.log(decodeURIComponent(url));
+
+
+
+// eval
+// let msg = 'hello world'
+// eval('console.log(msg);') // 进行了打印
+
+
+// eval("function sayHi() {console.log('hi');}")
+// sayHi() // hi
+
+// eval("let msg = 'hello'") // let 块级作用域，不会在eval外部访问到
+// console.log(msg); // Reference Error
+
+// console.log(msg); // Reference Error
+// eval("var msg = 'hello'") // let 块级作用域，不会在eval外部访问到
+// console.log(msg); // hello
+
+// "use strict"
+// eval = 'hi' // SyntaxError
+
+
+// var color = 'red'
+// function sayColor() {
+//   console.log(window.color);
+// }
+// window.sayColor() //red Node环境并没有window对象
+
+// console.log(this.toString()); // [object window] 浏览器环境下
+
+
+
+
+// Math
+// const numbers = [3, 2, 3, 4, 5, 67, 29, 1, 0.1]
+// let max = Math.max(...numbers) //67
+// let min = Math.min(...numbers) //0.1
+
+
+// console.log(Math.ceil(25.9)); //26
+// console.log(Math.ceil(25.5)); //26
+// console.log(Math.ceil(25.1)); //26
+// console.log(Math.floor(25.9)); //25
+// console.log(Math.floor(25.5)); //25
+// console.log(Math.floor(25.1)); //25
+// console.log(Math.round(25.9)); //26
+// console.log(Math.round(25.5)); //26
+// console.log(Math.round(25.1)); //25
+// console.log(Math.fround(.4)); //0.4000000059604645
+// console.log(Math.fround(.5));  // 0.5
+// console.log(Math.fround(25.1)); // 25.100000381469727
+
+
+
+// let num = Math.floor(Math.random() * 10 + 1) //1-10的随机整数
+// console.log(num);
+
+
+// function selectRange(lowerValue, upperValue) {
+//   let choices = upperValue - lowerValue + 1
+//   return Math.floor(Math.random() * choices + lowerValue)
+// }
+// console.log(selectRange(2, 10));// 2-10
+
+
+
+// 其他Math方法
+console.log(Math.abs(-1)); // 1
+console.log(Math.exp(0));  // 1 e^x  
+console.log(Math.expm1(0)); // 0 e^x-1
+console.log(Math.log(Math.E)); // 0 
+console.log(Math.log1p(0)); // 0  Math.log(x+1)
+console.log(Math.pow(2, 3)); // 8 2^3
+const nums = [2, 4, 4]
+console.log(Math.hypot(...nums)); //6 nums的平方和的平方根 
+console.log(Math.clz32(12)); //28  返回32位整数x的前置零的数量
+console.log(Math.sign(-10000)); // -1 表示x的符号 1,0,-0,-1
+console.log(Math.trunc(100.293)); // 100 返回整数部分，删除小数部分
+console.log(Math.sqrt(4)); //2  平方根
+console.log(Math.cbrt(2)); //8  立方根
+console.log(Math.acos(Math.cos(2))); //2 反余弦与余弦
+console.log(Math.tan(Math.PI / 4)); // 0.999999999 1 ?
+console.log(Math.sin(Math.PI / 2));// 1
